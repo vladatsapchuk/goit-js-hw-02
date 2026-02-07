81de9c4978fbdf36e5312ce6accdc4ed9de36eed
vladatsapchuk <vladatsapchuk@gmail.com>
2026-02-07 13:53:15 +0200

    solved tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,18 @@
-function getElementWidth (content, padding, border) {
- let contentWidth = parseFloat(content);
- let paddingWidth = parseFloat(padding) * 2; 
- let borderWidth = parseFloat(border) * 2;   
-
- return contentWidth + paddingWidth + borderWidth;
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+        return true;
+    } 
+    else {
+        return false;
+    }
 }
 
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
